46bcebb7773e134fd49e848c144097f95348b208
George Distelhurst <mrdeliteservices@gmail.com>
2025-12-16 08:23:55 -0500

    Restore Gears route as stub screen

---
+++ app/screens/gears.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Gears() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Gears (stub)</Text>
+    </View>
+  );
+}
