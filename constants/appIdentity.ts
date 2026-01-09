d60c760697db4d7687954eb7bf07c55d3d2bb507
George Distelhurst <mrdeliteservices@gmail.com>
2026-01-09 15:00:12 -0500

    Add APP_IDENTITY constants

---
+++ constants/appIdentity.ts
@@ -0,0 +1,9 @@
+// constants/appIdentity.ts
+// Single source of truth for app identity strings.
+// Update these values when cloning shells.
+
+export const APP_IDENTITY = {
+  displayName: "Fidget Frenzy",
+  slug: "fidget-frenzy",
+  scheme: "fidgetfrenzy",
+} as const;
