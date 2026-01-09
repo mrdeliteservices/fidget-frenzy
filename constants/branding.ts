5f57180406e9533356889d6e4c7d5b975c774c84
George Distelhurst <mrdeliteservices@gmail.com>
2026-01-09 15:14:26 -0500

    Add App Store display name constant

--- constants/branding.ts
+++ constants/branding.ts
@@ -1,4 +1,8 @@
 // constants/branding.ts
 
+// Apple/App Store/TestFlight visible display name (until trademarks clear)
+export const APP_STORE_DISPLAY_NAME = "Fidget Frenzy Elite";
+
+// Brand attribution shown inside the app
 export const BRAND_ATTRIBUTION =
   "Created by Frenzy Apps, a brand of MRD Elite Services, LLC";
